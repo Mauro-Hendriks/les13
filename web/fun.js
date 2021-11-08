ce3a122ae18e0eb0cdf28c50dfa8edfa03d164e7
Mauro-Hendriks <33409@ma-web.nl>
2021-11-08 17:56:06 +0100

    full lesson 13

---
+++ web/fun.js
@@ -0,0 +1,19 @@
+function change1() {
+    const hideMe = document.getElementById("hideme");
+    hideMe.style.display = "none";
+}
+
+function change2(){
+    const hidec = document.getElementById("hidec");
+    document.body.style.background = "red";
+}
+
+function change3(){
+    const moveme = document.getElementById("moveme");
+    moveme.innerText ="WHY???";
+}
+
+function change4(){
+    const movem = document.getElementById("movem");
+    movem.style.top ="200px";
+}    
